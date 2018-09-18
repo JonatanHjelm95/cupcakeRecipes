0db13670cbe24a436cd1b9d3a05b8e6e26f8f6ee
mf226 <cph-mf226@cphbusiness.dk>
2018-09-18 12:47:05 +0200

    added first DTO's

---
+++ src/main/java/logic/Recipe.java
@@ -0,0 +1,16 @@
+package logic;
+
+import java.util.ArrayList;
+
+public class Recipe {
+    private ArrayList<Ingredient> incredients = new ArrayList<>();
+    private String instructions;
+
+    public Recipe(String instructions) {
+        this.instructions = instructions;
+    }
+    
+    public void addIngredient() {
+        
+    }
+}
